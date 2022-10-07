4b90e37ec3365cdc585c065a883f615c213b4057
ShivShah202000 <112986630+ShivShah202000@users.noreply.github.com>
2022-10-07 18:45:33 -0400

    Update git.py

--- git.py
+++ git.py
@@ -5,6 +5,9 @@ import json
 def numberReposandCommits(userID):
     url = "https://api.github.com/users/" + userID + "/repos"
     req = requests.get(url)
+    if req.status_code != 200:  #checks if userID is not Valid
+        print("User Invalid")
+        return False
     content = req.json()
     repositories = []
     for REPO in content:
@@ -13,7 +16,8 @@ def numberReposandCommits(userID):
         commits = len(req2.json())
         repositories.append({"Repo":REPO["name"], "Number of commits": commits})
         print("Repo: " + REPO["name"] + " Number of commits: " + str(commits))        
-    return REPO
+    return True  # if there reposatory on same name, returns True and prints repositories in it.
+        
 
-user = "ShivShah202000"
-numberReposandCommits(user)
+#user = "ShivShah202000"
+#numberReposandCommits(user)
